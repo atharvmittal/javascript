98607b3a6f2740b5200a45ea91f5352490f1ab16
atharvmittal <atharv.22b0101030@abes.ac.in>
2025-04-19 15:45:33 +0530

    jsLearning

--- iterations/five.js
+++ iterations/five.js
@@ -1,4 +1,39 @@
-const arr=[3,4,5,6,7,8]
-arr.forEach(element => {
-    console.log(element)
-});
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach( function (val){
+//     console.log(val);
+// } )
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach( (item, index, arr)=> {
+//     console.log(item, index, arr);
+// } )
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach( (item) => {
+    
+    console.log(item.languageName);
+} )
